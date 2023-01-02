e2be993576b92e33f033f09712a9f5a5d128b04e
anmolsingh0001 <anmol.singh@kalvium.community>
2023-01-02 19:47:59 +0530

    pro-nutrition

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// import Pronutrition from './components/pro-nutrition';
+import FoodData from './resources/FoodData';
+import Hooks from './components/hooks';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Pronutrition test={FoodData}/> */}
+      <Hooks test={FoodData}/>
     </div>
   );
 }
---
+++ src/components/hooks.js
@@ -0,0 +1,185 @@
+
+
+
+
+
+import React,{useState,useRef,useEffect} from 'react'
+import data from '../resources/FoodData';
+
+function Hooks() {
+
+    
+
+    const [valu,test] = useState('');
+    // // const [change,check] = useState(0);
+
+    // // console.log(valu)
+
+    // const output=(elt,alt)=>{
+    //     let result=elt*alt;
+    //     return result;
+    // }
+
+
+    const firstref = useRef();
+    const secondref = useRef();
+    const thirdref = useRef();
+
+    const check=(id,cal)=>{
+
+      document.getElementById(`cal${id}`).innerHTML=document.getElementById(`set${id}`).value*cal;
+
+    }
+
+    // useEffect(()=>{
+    //  right();
+    // },[])
+
+    const reset=(id)=>{
+      document.getElementById(`cal${id}`).innerHTML='';
+      document.getElementById(`${id}`).innerHTML='';
+      document.getElementById(`set${id}`).value='';
+    }
+
+
+  
+    const right=(e,id)=>{
+      test(e.target.value);
+      document.getElementById(`${id}`).innerHTML=document.getElementById(`set${id}`).value;
+     
+      }
+     
+    
+
+    // console.log(firstref.current.id)
+    // console.log(secondref.current)
+
+    // const change=()=>{
+    //   let set=document.getElementsByClassName("todo");
+    //   set.classList.add("to");
+    // }
+
+      
+
+    // const last=()=>{
+    //   data.map((elt)=>
+    //   console.log(elt.img))
+    // }
+    // console.log(last())
+
+    const [third, setthrid] = useState('');
+
+    const manual=(e)=>{
+      
+        setthrid(e.target.value)
+    }
+
+    return (
+      <div>
+        <div>Search</div>
+
+        <div>
+          <input placeholder='search here' onChange={(e)=>manual(e)} value={third} id='search'></input>
+        </div>
+        {
+            data.map((elt)=>{
+              if(third===''){
+                return(
+                  
+                  <div ref={firstref} className={`main${elt.id}`}>
+                  <div className='set'>
+                      <div className='padding'>
+                      <img className='img' src={elt.img} alt='img' />
+                      </div>
+                      <div>
+                          <div>{elt.name}</div>
+                          <div>{elt.cal}</div>
+                      </div>
+                      <div className='input'>
+                        <div >
+                          <input id={`set${elt.id}`}  type='number' placeholder='type the quantity'  onChange={(e)=>right(e,elt.id)} />
+                        </div>
+                        <div>
+                          <button onClick={()=>check(elt.id,elt.cal)} >+</button>
+                        </div>
+                      </div>
+                  </div>
+                  <div className='item'>
+                    <div ref={secondref} className={`quantity${elt.id}`} id={`${elt.id}`}>
+                      
+                    </div>
+                    <div>
+                    {elt.name}
+                    </div>
+                    <div>
+                      =
+                    </div>
+                    <div id={`cal${elt.id}`}>
+
+                    </div>
+                    <div>
+                    calries
+                    </div>
+                    <div className='reset'>
+                      <button onClick={()=>reset(elt.id)}>Reset</button>
+                    </div>
+                  </div>
+                  </div>
+                  
+              )
+              }
+              if(elt.name[0]===third[0] && elt.name[1]===third[1]){
+                
+                return(
+                  
+                    <div ref={firstref} className={`main${elt.id}`}>
+                    <div className='set'>
+                        <div className='padding'>
+                        <img className='img' src={elt.img} alt='img' />
+                        </div>
+                        <div>
+                            <div>{elt.name}</div>
+                            <div>{elt.cal}</div>
+                        </div>
+                        <div className='input'>
+                          <div >
+                            <input id={`set${elt.id}`}  type='number' placeholder='type the quantity'  onChange={(e)=>right(e,elt.id)} />
+                          </div>
+                          <div>
+                            <button onClick={()=>check(elt.id,elt.cal)} >+</button>
+                          </div>
+                        </div>
+                    </div>
+                    <div className='item'>
+                      <div ref={secondref} className={`quantity${elt.id}`} id={`${elt.id}`}>
+                        
+                      </div>
+                      <div>
+                      {elt.name}
+                      </div>
+                      <div>
+                        =
+                      </div>
+                      <div id={`cal${elt.id}`}>
+
+                      </div>
+                      <div>
+                      calries
+                      </div>
+                      <div className='reset'>
+                        <button onClick={()=>reset(elt.id)}>Reset</button>
+                      </div>
+                    </div>
+                    </div>
+                    
+                )
+              
+            }
+            })
+        }
+      </div>
+    )
+}
+
+
+export default Hooks;
